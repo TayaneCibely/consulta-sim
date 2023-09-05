fb9aece699a80b60f9189331f1f3566d654d9702
TayaneCibely <tayanerodrigues2001@gmail.com>
2023-09-05 20:45:27 -0300

    feature paciente:cucumber

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module ConsultaSim
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
     config.autoload_paths += %W(#{config.root}/app/helpers)
 
     # Configuration for the application, engines, and railties goes here.
